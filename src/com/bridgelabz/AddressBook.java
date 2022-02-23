ac4de425a9fb3dd352b10e2dc9fd57e5f34eca3f
pragnakasthruri <kpragna25@gmail.com>
2022-02-23 14:55:02 +0530

    Welcome to Address Book Program added

---
+++ src/com/bridgelabz/AddressBook.java
@@ -0,0 +1,11 @@
+/**
+ * Start with Displaying
+ * Welcome to Address Book Program
+ */
+package com.bridgelabz;
+
+public class AddressBook {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book program...!");
+    }
+}
