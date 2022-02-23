c5622c0c59cfeec51b5656f1ea900163904f9fe4
pragnakasthruri <kpragna25@gmail.com>
2022-02-23 19:53:45 +0530

    Ability to delete a person using person's name UC4

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -5,6 +5,7 @@
 package com.bridgelabz;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -12,15 +13,14 @@ public class AddressBook {
     /**
      * Creating a list to store Contact Details
      */
-    private  static Scanner scanner = new Scanner(System.in);
+    private static Scanner scanner = new Scanner(System.in);
 
     private static List<Contact> contactList = new ArrayList<>();
 
     /**
-     * @param scanner
-     * Method to take user input to add details in contact
+     * @param scanner Method to take user input to add details in contact
      */
-    public static void addNewContact(Scanner scanner){
+    public static void addNewContact(Scanner scanner) {
         System.out.println("Please enter the details :");
         System.out.println("First Name :");
         String firstName = scanner.next();
@@ -43,12 +43,12 @@ public class AddressBook {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book program...!");
-        String userChoice = "N";
+        String userChoice;
         do {
             readUserInput(scanner);
             System.out.println("Do you want to continue(Y/N) ?");
             userChoice = scanner.next();
-        }while(userChoice.equals("Y"));
+        } while (userChoice.equals("Y"));
         System.out.println("Thank you!");
     }
 
@@ -58,7 +58,7 @@ public class AddressBook {
      */
     private static void readUserInput(Scanner scanner) {
         System.out.println("Please select one option");
-        System.out.println("1. Create new contact \n2. Edit contact \n3. List contacts");
+        System.out.println("1. Create new contact \n2. Edit contact \n3. List contacts \n4. Delete contact ");
         int userChoice = scanner.nextInt();
         switch (userChoice) {
             case 1:
@@ -71,71 +71,90 @@ public class AddressBook {
             case 3:
                 listContacts();
                 break;
+            case 4:
+                deleteContact();
+                break;
             default:
                 System.out.println("Invalid option. Please select valid");
         }
     }
 
     /**
-     * Display the list
+     * Method for deleting the contact for the given name
      */
-    private static void listContacts() {
-        System.out.println(contactList);
-    }
-
-    /**
-     * This method is used to give the user edit option
-     */
-    private static void editContact() {
-        System.out.println("Please enter the name of the person u want to edit :");
-        String searchName = scanner.next();
-        for(Contact contact : contactList){
-            if (searchName.equals(contact.getFirstName())){
-                System.out.println("Please select the field you want to edit ");
-                System.out.println("1.Lastname  2.Address  3.City  4.State  5.Zip  6.Phone number  7.email ");
-                int userChoice = scanner.nextInt();
-                switch(userChoice){
-                    case 1:
-                        System.out.println("Please enter new last name :");
-                        String lastName = scanner.next();
-                        contact.setLastName(lastName);
-                        break;
-                    case 2:
-                        System.out.println("Please enter new address :");
-                        String address = scanner.next();
-                        contact.setAddress(address);
-                        break;
-                    case 3:
-                        System.out.println("Please enter new city :");
-                        String city = scanner.next();
-                        contact.setCity(city);
-                        break;
-                    case 4:
-                        System.out.println("Please enter new state :");
-                        String state = scanner.next();
-                        contact.setState(state);
-                        break;
-                    case 5:
-                        System.out.println("Please enter new zip :");
-                        long zip = scanner.nextLong();
-                        contact.setZip(zip);
-                        break;
-                    case 6:
-                        System.out.println("Please enter new phone number :");
-                        long phoneNumber = scanner.nextLong();
-                        contact.setPhoneNumber(phoneNumber);
-                        break;
-                    case 7:
-                        System.out.println("Please enter new email :");
-                        String email = scanner.next();
-                        contact.setEmail(email);
-                        break;
-                    default:
-                        System.out.println("Invalid option. Please enter valid !");
-                }
+    private static void deleteContact() {
+        System.out.println("Please enter the name of the person u want to delete :");
+        String contactName = scanner.next();
+        Iterator<Contact> iterator = contactList.iterator();
+        while(iterator.hasNext()){
+            Contact contact = iterator.next();
+            if(contactName.equals(contact.getFirstName())){
+                iterator.remove();
             }
         }
         listContacts();
     }
-}
+        /**
+         * Display the list
+         */
+        private static void listContacts () {
+            System.out.println(contactList);
+        }
+
+        /**
+         * This method is used to give the user edit option
+         */
+        private static void editContact () {
+            System.out.println("Please enter the name of the person u want to edit :");
+            String searchName = scanner.next();
+            for (Contact contact : contactList) {
+                if (searchName.equals(contact.getFirstName())) {
+                    System.out.println("Please select the field you want to edit ");
+                    System.out.println("1.Lastname  2.Address  3.City  4.State  5.Zip  6.Phone number  7.email ");
+                    int userChoice = scanner.nextInt();
+                    switch (userChoice) {
+                        case 1:
+                            System.out.println("Please enter new last name :");
+                            String lastName = scanner.next();
+                            contact.setLastName(lastName);
+                            break;
+                        case 2:
+                            System.out.println("Please enter new address :");
+                            String address = scanner.next();
+                            contact.setAddress(address);
+                            break;
+                        case 3:
+                            System.out.println("Please enter new city :");
+                            String city = scanner.next();
+                            contact.setCity(city);
+                            break;
+                        case 4:
+                            System.out.println("Please enter new state :");
+                            String state = scanner.next();
+                            contact.setState(state);
+                            break;
+                        case 5:
+                            System.out.println("Please enter new zip :");
+                            long zip = scanner.nextLong();
+                            contact.setZip(zip);
+                            break;
+                        case 6:
+                            System.out.println("Please enter new phone number :");
+                            long phoneNumber = scanner.nextLong();
+                            contact.setPhoneNumber(phoneNumber);
+                            break;
+                        case 7:
+                            System.out.println("Please enter new email :");
+                            String email = scanner.next();
+                            contact.setEmail(email);
+                            break;
+                        default:
+                            System.out.println("Invalid option. Please enter valid !");
+                    }
+                }
+            }
+            listContacts();
+        }
+    }
+
 
